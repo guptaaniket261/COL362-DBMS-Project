ebfe2ae9c9a4627e4acc3728fa9ab926d91224a2
guptaaniket261 <guptaaniket261@gmail.com>
2022-02-10 13:52:31 +0530

    project started

---
+++ app.py
@@ -0,0 +1,32 @@
+import os
+import psycopg2
+from flask import Flask, render_template
+from config import credentials
+
+
+app = Flask(__name__)
+
+def get_db_connection():
+    """
+    Returns a connection to the database
+    """
+    conn = psycopg2.connect(
+      host = credentials['host'],
+      database = credentials['database'],
+      user = credentials['user'],
+      password = credentials['password']
+    )
+
+    return conn
+
+@app.route('/')
+def index():
+  conn = get_db_connection()
+  cur = conn.cursor()
+  cur.execute("SELECT * FROM drivers limit 10")
+  drivers = cur.fetchall()
+  return render_template('index.html', drivers=drivers)
+
+
+if __name__ == '__main__':
+  app.run(debug=True)
