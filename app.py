9f3ef5a2842c39efce894eced640f6396e6b661b
Hardeep Kaur <hardeepkaur@Hardeeps-MacBook-Air.local>
2022-02-21 17:43:49 +0530

    user_profile

--- app.py
+++ app.py
@@ -27,6 +27,14 @@ def index():
   drivers = cur.fetchall()
   return render_template('index.html', drivers=drivers)
 
+@app.route('/profile')
+def get():
+  # conn = get_db_connection()
+  # cur = conn.cursor()
+  # cur.execute("SELECT * FROM drivers limit 10")
+  # drivers = cur.fetchall()
+  return render_template('user_profile.html')
+
 
 if __name__ == '__main__':
   app.run(debug=True)
