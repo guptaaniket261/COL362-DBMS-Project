144281a9bcd1f824f56edcdf74c40f568903c307
Nausheen15 <mdjnausheen786@gmail.com>
2022-02-27 00:38:35 +0530

    changes to datasets, applications added

--- tables/schema.sql
+++ tables/schema.sql
@@ -26,27 +26,39 @@ CREATE table IF NOT EXISTS job_details(
     status bit
 ); 
 
+
 \copy job_details from tables/job_details.csv DELIMITER ',' CSV HEADER;
 
 
--- CREATE TABLE IF NOT EXISTS user_details(
---     user_id bigint PRIMARY KEY,
---     firstname text,
---     lastname text,
---     age smallint,
---     gender CHAR(1),
---     ethnicity text,
---     address text,
---     /*experiences*/  
---     email text,
---     contact char(10)
--- );
--- CREATE TABLE IF NOT EXISTS applications(
---     application_id bigint PRIMARY KEY,
---     job_id bigint REFERENCES job_details(job_id),
---     user_id bigint REFERENCES user_details(user_id),
---     status smallint
--- );
+ CREATE TABLE IF NOT EXISTS user_details(
+     user_id bigint PRIMARY KEY,
+     firstname text,
+     lastname text,
+     age smallint,
+     gender CHAR(1),
+     ethnicity text,
+     address text,
+     email text,
+     contact char(10)
+ );
 
--- INSERT INTO user_details(user_id, firstname, lastname, contact) VALUES(1, 'Hello', 'World', '0000000000');
--- INSERT INTO applications VALUES(1,16349, 1, 1);
+ CREATE TABLE IF NOT EXISTS experiences(
+    exp_id bigint PRIMARY KEY,
+    start_year bigint,
+    years bigint,
+    role text,
+    user_id bigint REFERENCES user_details(user_id),
+
+);
+
+ /*
+ CREATE TABLE IF NOT EXISTS applications(
+     application_id bigint PRIMARY KEY,
+     job_id bigint REFERENCES job_details(job_id),
+     user_id bigint REFERENCES user_details(user_id),
+     status smallint
+ );
+
+ INSERT INTO user_details(user_id, firstname, lastname, contact) VALUES(1, 'Hello', 'World', '0000000000');
+ INSERT INTO applications VALUES(1,16349, 1, 1);
+ */
